0e3dc5eac9915c77c919832621f740490c9b6b58
Andreas S <Andreas237@users.noreply.github.com>
2019-02-28 10:37:46 -0700

    Update master.sh

--- DecompilationScript/master.sh
+++ DecompilationScript/master.sh
@@ -1,14 +1,27 @@
 #!/bin/bash
 
 
-#1 Remove existing directory for output
+
+############################################
+#               Part 1:The APK             #
+############################################
+# Use APK tool to decompile the APK which is
+# passed as parameter 1 to the script
+./../Tools/dex2jar-2.0/d2j-dex2jar.sh
 
 
 
 
 
-#2 Run Apktool to decompile whatever APK is in this directory
-apktool d *.apk
+
+###########################################
+#               Part 2: The Jar           #
+###########################################
+# The jar must be decompiled.  In this pro-
+# ect we use JAD
 
 
-#3 ... Replace, search build???
+
+
+
+# Code
